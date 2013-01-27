aba76c19235e5b5606dbec61c29e61d6715c31a1
Caleb Fultz <calebfultz@gmail.com>
2013-01-27 15:43:19 -0500

    Updated Layout
    Updated the layout to display the ad correctly as well as added the
    settings.xml for later

--- gen/us/autumnbomb/rootcheck/R.java
+++ gen/us/autumnbomb/rootcheck/R.java
@@ -20,20 +20,20 @@ public final class R {
         public static final int splash=0x7f020006;
     }
     public static final class id {
-        public static final int about=0x7f07000c;
-        public static final int adView=0x7f07000a;
-        public static final int apache=0x7f070005;
-        public static final int apache2=0x7f070001;
-        public static final int apacheTitle=0x7f070004;
-        public static final int busybox=0x7f070009;
-        public static final int imageView=0x7f070002;
-        public static final int isAccessGiven=0x7f070006;
-        public static final int isRootAvailable=0x7f070007;
-        public static final int isbusybox=0x7f070008;
-        public static final int library=0x7f070003;
-        public static final int license=0x7f070000;
-        public static final int share=0x7f07000d;
-        public static final int textView=0x7f07000b;
+        public static final int about=0x7f08000c;
+        public static final int adView=0x7f08000a;
+        public static final int apache=0x7f080005;
+        public static final int apache2=0x7f080001;
+        public static final int apacheTitle=0x7f080004;
+        public static final int busybox=0x7f080009;
+        public static final int imageView=0x7f080002;
+        public static final int isAccessGiven=0x7f080006;
+        public static final int isRootAvailable=0x7f080007;
+        public static final int isbusybox=0x7f080008;
+        public static final int library=0x7f080003;
+        public static final int license=0x7f080000;
+        public static final int share=0x7f08000d;
+        public static final int textView=0x7f08000b;
     }
     public static final class layout {
         public static final int about=0x7f030000;
@@ -41,20 +41,20 @@ public final class R {
         public static final int splash=0x7f030002;
     }
     public static final class menu {
-        public static final int menu=0x7f060000;
+        public static final int menu=0x7f070000;
     }
     public static final class string {
-        public static final int apache=0x7f040009;
-        public static final int apache2=0x7f04000a;
-        public static final int app_name=0x7f040006;
-        public static final int busybox=0x7f040001;
-        public static final int exampleVersion=0x7f040000;
-        public static final int isAccessGiven=0x7f040004;
-        public static final int isRootAvailable=0x7f040003;
-        public static final int isbusybox=0x7f040002;
-        public static final int library=0x7f040005;
-        public static final int license=0x7f040007;
-        public static final int roottools=0x7f040008;
+        public static final int apache=0x7f050009;
+        public static final int apache2=0x7f05000a;
+        public static final int app_name=0x7f050006;
+        public static final int busybox=0x7f050001;
+        public static final int exampleVersion=0x7f050000;
+        public static final int isAccessGiven=0x7f050004;
+        public static final int isRootAvailable=0x7f050003;
+        public static final int isbusybox=0x7f050002;
+        public static final int library=0x7f050005;
+        public static final int license=0x7f050007;
+        public static final int roottools=0x7f050008;
     }
     public static final class style {
         /** 
@@ -78,10 +78,13 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f060001;
+    }
+    public static final class xml {
+        public static final int settings=0x7f040000;
     }
 }
