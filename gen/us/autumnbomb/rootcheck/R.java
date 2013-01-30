9bcb2e433349f52540a373787a95ea4ace858e0e
Caleb Fultz <calebfultz@gmail.com>
2013-01-30 01:18:15 -0500

    Corrected strings, layouts, version number
    Corrected a couple of the strings to make them a little easier to read.
    Corrected layouts of the About.java/about.xml to make them work again.
    Added android:contentDescritpion to splash screen ImageView

--- gen/us/autumnbomb/rootcheck/R.java
+++ gen/us/autumnbomb/rootcheck/R.java
@@ -52,20 +52,22 @@ public final class R {
         public static final int menu=0x7f070000;
     }
     public static final class string {
-        public static final int apache=0x7f05000c;
-        public static final int apache2=0x7f05000d;
-        public static final int app_name=0x7f050009;
-        public static final int busybox=0x7f050001;
-        public static final int copyright=0x7f050005;
-        public static final int exampleVersion=0x7f050000;
-        public static final int isAccessGiven=0x7f050004;
-        public static final int isRootAvailable=0x7f050003;
-        public static final int isbusybox=0x7f050002;
-        public static final int library=0x7f050008;
-        public static final int license=0x7f05000a;
-        public static final int roottools=0x7f05000b;
-        public static final int roottoolsview=0x7f050007;
-        public static final int view=0x7f050006;
+        public static final int Version=0x7f050000;
+        public static final int apache=0x7f05000d;
+        public static final int apache2=0x7f05000e;
+        public static final int app_name=0x7f05000a;
+        public static final int busybox=0x7f050002;
+        public static final int copyright=0x7f050006;
+        public static final int exampleVersion=0x7f05000f;
+        public static final int isAccessGiven=0x7f050005;
+        public static final int isRootAvailable=0x7f050004;
+        public static final int isbusybox=0x7f050003;
+        public static final int library=0x7f050009;
+        public static final int license=0x7f05000b;
+        public static final int logoDesc=0x7f050001;
+        public static final int roottools=0x7f05000c;
+        public static final int roottoolsview=0x7f050008;
+        public static final int view=0x7f050007;
     }
     public static final class style {
         /** 
